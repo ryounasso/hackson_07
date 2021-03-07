17c6d3adaf4fbe497645c88fd80138fe291d9fa3
ryonasso <ryounaoyuki@gmail.com>
2021-03-07 18:35:05 +0900

    fix requirements.txt

--- hackson_07/settings.py
+++ hackson_07/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from socket import gethostname
-import environ
+from os import environ
 import os
 
 HOSTNAME = gethostname()

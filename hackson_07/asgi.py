40f6a160655be9d6574dc636348303dcac9586ec
ryonasso <ryounaoyuki@gmail.com>
2021-03-09 18:59:42 +0900

    fix

--- hackson_07/asgi.py
+++ hackson_07/asgi.py
@@ -16,15 +16,16 @@ import django
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackson_07.settings')
-
 django.setup()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackson_07.settings')
+
+
 application = get_asgi_application()
-# django_asgi_app = get_asgi_application()
+django_asgi_app = get_asgi_application()
 
 
-# application = ProtocolTypeRouter({
-#     'http': django_asgi_app,
-#     'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
-# })
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+})

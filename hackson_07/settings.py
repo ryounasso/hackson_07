ea39f253f8ee0dcd365b57cfef53fdcaf91fbf3f
ryonasso <ryounaoyuki@gmail.com>
2021-03-09 16:07:21 +0900

    fix typo

--- hackson_07/settings.py
+++ hackson_07/settings.py
@@ -141,7 +141,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.enbiron.get('REDIS_URL', 'redis://localhost:6379')],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     }
 }

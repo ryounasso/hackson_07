0d0b5873f61466b97ad52d0d911618083b2b1d6c
ryonasso <ryounaoyuki@gmail.com>
2021-03-09 15:54:33 +0900

    fix path

--- static/js/chat.js
+++ static/js/chat.js
@@ -28,11 +28,11 @@ const imgListElemnt = document.getElementById("image_space");
 let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
 const g_socket = new WebSocket(
   // ws_scheme + "://" + window.location.host + "/wss/chat/"
-  ws_scheme + "://guarded-eyrie-48747.herokuapp.com/" + "/wss/chat/"
+  ws_scheme + "://guarded-eyrie-48747.herokuapp.com/" + "wss/chat/"
 );
 const g_socket_image = new WebSocket(
   // ws_scheme + "://" + window.location.host + "/wss/chat/"
-  ws_scheme + "://guarded-eyrie-48747.herokuapp.com/" + "/wss/chat/"
+  ws_scheme + "://guarded-eyrie-48747.herokuapp.com/" + "wss/chat/"
 );
 
 // 「Join」ボタンを押すと呼ばれる関数

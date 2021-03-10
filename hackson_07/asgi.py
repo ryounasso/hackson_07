a51c365f58da5c7a0ecf49e0b2015c14fb106458
ryonasso <ryounaoyuki@gmail.com>
2021-03-10 17:34:50 +0900

    nc

--- hackson_07/asgi.py
+++ hackson_07/asgi.py
@@ -57,6 +57,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackson_07.settings')
 django.setup()
 application = get_default_application()

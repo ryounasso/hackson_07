e6c5b3cc1f8db1ee647dad677c7222943bde96a8
ryonasso <ryounaoyuki@gmail.com>
2021-03-10 17:31:00 +0900

    nc

--- hackson_07/asgi.py
+++ hackson_07/asgi.py
@@ -7,23 +7,44 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
+# from channels.routing import URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter
+# import os
+# import chat.routing
+# import django
+
+# from django.core.asgi import get_asgi_application
+# from channels.routing import get_default_application
+
+# django.setup()
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackson_07.settings')
+
+# # application = get_default_application()
+
+# # application = get_asgi_application()
+# django_asgi_app = get_asgi_application()
+
+
+# application = ProtocolTypeRouter({
+#     'http': django_asgi_app,
+#     'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+# })
+
+
 from channels.routing import URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 import os
 import chat.routing
-import django
 
 from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
-
-django.setup()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackson_07.settings')
 
-# application = get_default_application()
 
-# application = get_asgi_application()
+#application = get_asgi_application()
 django_asgi_app = get_asgi_application()
 
 
